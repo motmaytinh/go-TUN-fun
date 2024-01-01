c583315e6b9e9e263be394f5feb1e64a5cf2b25a
quytn <quytn@vng.com.vn>
2024-01-01 18:50:19 +0700

    feat: add decode packet function

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/motmaytinh/go-tun-fun
 
 go 1.20
 
-require github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
+require (
+	github.com/google/gopacket v1.1.19
+	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
+)
 
 require golang.org/x/sys v0.15.0 // indirect
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
-
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/songgao/water"
 )
 
@@ -23,5 +25,16 @@ func main() {
 			log.Fatal(err)
 		}
 		log.Printf("Packet Received: % x\n", packet[:n])
+		decodedPacket := gopacket.NewPacket(packet[:n], layers.LayerTypeICMPv4, gopacket.Default)
+		if tcpLayer := decodedPacket.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+			fmt.Println("This is a TCP packet!")
+			// Get actual TCP data from this layer
+			tcp, _ := tcpLayer.(*layers.TCP)
+			fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
+		}
+		// Iterate over all layers, printing out each layer type
+		for _, layer := range decodedPacket.Layers() {
+			fmt.Println("PACKET LAYER:", layer.LayerType())
+		}
 	}
 }
